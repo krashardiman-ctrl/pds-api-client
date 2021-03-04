b157b9003b5291e725e15309ef4d1e3bc4327b25
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2021-03-04 14:02:07 -0800

    update swagger specification

--- client-demo.py
+++ client-demo.py
@@ -6,11 +6,11 @@ from pprint import pprint
 
 # create an instance of the API class
 configuration = Configuration()
+#configuration.host = 'http://pds-gamma.jpl.nasa.gov/api/'
 configuration.host = 'http://localhost:8080/'
 api_instance = pds_api_client.CollectionsApi(pds_api_client.ApiClient(configuration))
 
 try:
-    # capabilities api entry point, list the resources provided by the current API end-point.
     api_response = api_instance.get_collection(q="", start=0, limit=20)
     pprint(api_response)
 except ApiException as e:
